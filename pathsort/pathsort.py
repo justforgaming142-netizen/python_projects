ff37f096f5ea06ae3ae6cea18955ce0fafb5b1d1
daniel <justforgaming142@gmail.com>
2026-04-19 10:34:00 +0530

    made some twicks: doesnt ask for consent. no flashy stuff.

--- pathsort/pathsort.py
+++ pathsort/pathsort.py
@@ -84,7 +84,6 @@ avail_formats=[]
 def listitems(count=0):
     for item in cwd.iterdir():
         print(item.name)
-        sleep(.1)
         count+=1
         avail_files.append(item)
         if item.suffix and item.suffix not in avail_formats:
@@ -92,34 +91,25 @@ def listitems(count=0):
 
     print(f'total  number of items... {count}')
     print(f'formats file formats found... \n{avail_formats}')
-    print(f'formats files found... \n{avail_files}')
 
 # === makes directories ===
 def mkdir():
-        shalli=consent("""
-        do you want to make the folllowing directories? y/n
-        """)
-        if shalli:
-            for items in avail_formats:
-                folder = file_map.get(items)
-                if folder is not None:
-                    Path(cwd/folder).mkdir(parents=True, exist_ok=True)
+        for items in avail_formats:
+            folder = file_map.get(items)
+            if folder is not None:
+                Path(cwd/folder).mkdir(parents=True, exist_ok=True)
 
 
 # === moves files to appropriate folders ===
 def movefiles():
-        shalli= consent('do you want to move files to appropriate folders? y/n \n')
-        if shalli:
-            for item in avail_files:
-                if item.is_file():
-                        folder = file_map.get(item.suffix)
-                        if folder is not None:
-                            des = cwd/folder/item.name
-                            item.rename(des)
-                        if folder is None:
-                            print(f'file not identified \n=====\n{item}\n=====\n')
-        elif not shalli:
-            print('items will not be moved')
+        for item in avail_files:
+            if item.is_file():
+                folder = file_map.get(item.suffix)
+                if folder is not None:
+                    des = cwd/folder/item.name
+                    item.rename(des)
+                if folder is None:
+                    print(f'file not identified \n=====\n{item}\n=====\n')
 
 
 
