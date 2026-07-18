e8806b88de673fb7737cf4611a85c07c216f4ef3
daniel <justforgaming142@gmail.com>
2026-07-18 15:48:45 +0530

    ## Finally moved to prompt_toolkit !! currently only using it for input autocomplition will format it more I still need to add method for adding accounts and account wise summaries

--- budget planner/btracker.py
+++ budget planner/btracker.py
@@ -11,9 +11,14 @@
 import json
 from pathlib import Path
 import time
+from prompt_toolkit import prompt
+from prompt_toolkit import print_formatted_text as print
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+
 # cached constants
 sum_total=0
-main_menu = ["Total Balance", "add an entry", "add an accounts", "delete/edit an entry"]
+main_menu = ["total Balance", "add an entry", "add an accounts", "delete/edit an entry"]
 
 
 data = {}
@@ -43,26 +48,34 @@ print(data)
 for option in main_menu:
     print(option)
 
-usr = input("what to do...?")
 
-usrr = main_menu[int(usr)-1]
+usrr = prompt(message="What to you want to do?", completer=WordCompleter(main_menu), complete_while_typing=True) 
+# usr = input("what to do...?")
 
-if usrr == 'Total Balance':
+# usrr = main_menu[int(usr)-1]
+
+if usrr == 'total Balance':
+    sum_total=0
     for items in data["entries"]:
-        if items['type']== 'paid':
+        if items['type']== 'credit':
             sum_total = sum_total - int(items["amount"])
-        elif items['type']== 'debited':
-            sum_total =+ int(items["amount"])
+        elif items['type']== 'debit':
+            sum_total = sum_total + int(items["amount"])
     print('The total balance is')
     print(f'=== {sum_total} ===')
 elif usrr == "add an entry":
     temp_data = {}
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    tran_type = input('credit or debit?…')
-    amount = input('how much?')
+    ss = PromptSession()    
+    tran_type = ss.prompt(message='credit or debit?')
+    amount = ss.prompt(message='how much?')
     temp_data.update({"date": current_time, "type": tran_type, "amount": amount})
     print(temp_data)
-    cons = input("confirm the following data?")
+    cons = ss.prompt(message='confirms?')
+    # tran_type = input('credit or debit?…')
+    # amount = input('how much?')
+    # print(temp_data)
+    # cons = input("confirm the following data?")
     if cons == 'y':
         data['entries'].append(temp_data)
     json_str = json.dumps(data, indent=4)
